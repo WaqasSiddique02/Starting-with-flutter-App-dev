1a8934143b56d48128735a6b640779fff0320685
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-09-19 10:56:25 +0500

    added styling to body font

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,14 @@ void main() {
       backgroundColor: Colors.blue,
     ),
     body: const Center(
-      child: Text('Geoo bhutoo'),
+      child: Text(
+        'Geoo bhutoo',
+        style: TextStyle(
+          fontSize: 30.0,
+          letterSpacing: 2.0,
+          color: Color.fromARGB(255, 167, 158, 158),
+        ),
+      ),
     ),
     floatingActionButton: const FloatingActionButton(
       onPressed: null,
