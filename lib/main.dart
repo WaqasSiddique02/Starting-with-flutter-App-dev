52454240fe92491131f0b7beb85ec3305acb4d30
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-09-19 11:12:55 +0500

    added home stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: const Text('Hello World'),
-      backgroundColor: Colors.blue,
-    ),
-    body: const Center(
-      child: Text(
-        'Geoo bhutoo',
-        style: TextStyle(
-          fontSize: 30.0,
-          letterSpacing: 2.0,
-          color: Color.fromARGB(255, 167, 158, 158),
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello World'),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text(
+          'Geoo bhutoo',
+          style: TextStyle(
+            fontSize: 30.0,
+            letterSpacing: 2.0,
+            color: Color.fromARGB(255, 167, 158, 158),
+          ),
         ),
       ),
-    ),
-    floatingActionButton: const FloatingActionButton(
-      onPressed: null,
-      backgroundColor: Colors.redAccent,
-      child: Text('click'),
-    ),
-  )));
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.blue,
+        child: Text('click'),
+      ),
+    );
+  }
 }
